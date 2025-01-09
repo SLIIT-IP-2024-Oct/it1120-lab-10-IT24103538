a43700ea5124ee941a5d386b149859a2fa99b7a4
IT24103538 <it24103538@my.sliit.lk>
2025-01-09 16:19:22 +0530

    Add files via upload

---
+++ Q1/IT24103538Lab10Q1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ public class IT24103538Lab10Q1 {
+   public static void main(String[]args){ 
+
+         Scanner scanner =new Scanner(System.in);
+         
+         System.out.println("ENTER MARKS");
+         int mark = scanner.nextInt();
+ 
+assert(mark>=0&&mark<=100):" Invalid Mark";
+ System.out.println("Mark is validated");
+}  
+}  
